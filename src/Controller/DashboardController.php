7215c43780929c460e34161206d655b35a717f80
Murat Akkus <akkusmurat123@gmail.com>
2025-04-28 06:25:17 +0200

    Home Page UI, Dashboard Overview UI, Dashboard Exercise Library UI, Dashboard Todays Workout UI, Dashboard My Programs UI and some Atomic App UI components are done.For first step they are just spaghetti coded yet.  Dashboard routes are done too.

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -8,7 +8,11 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/dashboard')]
 class DashboardController extends AbstractController
 {
+<<<<<<< HEAD
   #[Route('/','app_dashboard_overview')]
+=======
+  #[Route('/')]
+>>>>>>> 289bc00 (Home Page UI, Dashboard Overview UI, Dashboard Exercise Library UI, Dashboard Todays Workout UI, Dashboard My Programs UI and some Atomic App UI components are done.For first step they are just spaghetti coded yet.  Dashboard routes are done too.)
     public function index(): Response
     {
        return $this->render('user_dashboard/index.html.twig');
