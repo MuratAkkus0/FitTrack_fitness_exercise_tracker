a86414300970712673c5b80be3906ef7ee68d788
Murat Akkus <akkusmurat123@gmail.com>
2025-05-05 09:25:43 +0200

    The git difference comments were in the code file as text. Because of wrong git rebase practice they somehow converted themselves to text and stayed in code file as text. i deleted them.

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -8,32 +9,25 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/dashboard')]
 class DashboardController extends AbstractController
 {
-<<<<<<< HEAD
-<<<<<<< HEAD
-  #[Route('/','app_dashboard_overview')]
-=======
-  #[Route('/')]
->>>>>>> 289bc00 (Home Page UI, Dashboard Overview UI, Dashboard Exercise Library UI, Dashboard Todays Workout UI, Dashboard My Programs UI and some Atomic App UI components are done.For first step they are just spaghetti coded yet.  Dashboard routes are done too.)
-=======
-  #[Route('/','app_dashboard_overview')]
->>>>>>> c55a49d (Dashboard main route named as dashboard_overview. dashboard sidebars links are created and they are able to change their bg-color according to active page now)
-    public function index(): Response
-    {
-       return $this->render('user_dashboard/index.html.twig');
-    }
+
+  #[Route('/', 'app_dashboard_overview')]
+  public function index(): Response
+  {
+    return $this->render('user_dashboard/index.html.twig');
+  }
   #[Route('/today')]
-    public function todaysWorkout(): Response
-    {
-       return $this->render('user_dashboard/todays_workout/index.html.twig');
-    }
+  public function todaysWorkout(): Response
+  {
+    return $this->render('user_dashboard/todays_workout/index.html.twig');
+  }
   #[Route('/my-programs')]
-    public function programs(): Response
-    {
-       return $this->render('user_dashboard/my_programs/index.html.twig');
-    }
+  public function programs(): Response
+  {
+    return $this->render('user_dashboard/my_programs/index.html.twig');
+  }
   #[Route('/exercise-library')]
-    public function exerciseLibrary(): Response
-    {
-       return $this->render('user_dashboard/exercise_library/index.html.twig');
-    }
+  public function exerciseLibrary(): Response
+  {
+    return $this->render('user_dashboard/exercise_library/index.html.twig');
+  }
 }
