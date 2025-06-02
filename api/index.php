670d83baee1e3c9b9a582a5380ebaa18545c93b3
Murat Akkus <akkusmurat123@gmail.com>
2025-06-02 19:23:33 +0200

    Fix Vercel deployment by using proper Symfony Runtime format

--- api/index.php
+++ api/index.php
@@ -1,9 +1,12 @@
 <?php
 
+use Symfony\Component\HttpFoundation\Request;
 use App\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    return new Kernel($context['APP_ENV'] ?? 'prod', (bool) ($context['APP_DEBUG'] ?? false));
+    $kernel = new Kernel($context['APP_ENV'] ?? 'prod', (bool) ($context['APP_DEBUG'] ?? false));
+
+    return $kernel;
 };
